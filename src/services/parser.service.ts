b1c3f81702a873a11108b5bb9341bab1be0cfb0d
Shtefanika Vadym <101572293+vadymShtefanika@users.noreply.github.com>
2022-09-29 18:53:01 +0300

    Added functionality for automatic scrapping

--- src/services/parser.service.ts
+++ src/services/parser.service.ts
@@ -1,23 +1,71 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { RankedSite } from '../entities/rankedSite.entity';
 import { RankedSitesService } from './rankedSites.service';
+import * as puppeteer from 'puppeteer';
 
 @Injectable()
 export class ParserService {
-  constructor(
-    @InjectRepository(RankedSite)
-    private readonly rankedSitesService: RankedSitesService,
-  ) {}
+  constructor(private readonly rankedSitesService: RankedSitesService) {}
 
   async parseRecentNews(news: string[]) {
-    let rank = null;
+    let slicedNews;
+    const result = [];
+    if (news.length > 10) slicedNews = news.slice(0, 10);
+    else slicedNews = news;
 
-    // scrape the news articles, get the title & content and then process the info about each article
+    try {
+      const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 100,
+        devtools: false,
+      });
+      for (let i = 0; i < slicedNews.length; i++) {
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await Promise.all([page.goto(slicedNews[i]), page.waitForNavigation()]);
+        const pageData = await page.evaluate((url: string) => {
+          const pageDetails = {
+            url: url,
+            title: '',
+            content: '',
+          };
+          let content = '';
+          const title = document
+            .getElementsByTagName('h1')[0]
+            .textContent?.trim();
+          const pageElements = document.getElementsByTagName('DIV');
 
-    // save rankedSite in DB
-
-    return rank;
+          for (let i = 0; i < pageElements.length; i++) {
+            if (
+              pageElements[i].tagName === 'SECTION' ||
+              pageElements[i].classList.value.includes('article')
+            ) {
+              const contentNews = pageElements[i]?.textContent
+                ?.replace(/\s{2,}/g, ' ')
+                .trim();
+              if (content.length < contentNews.length) content = contentNews;
+            }
+          }
+          if (content === '') {
+            const articles = document.getElementsByTagName('ARTICLE');
+            for (let j = 0; j < articles.length; j++) {
+              const contentNews = articles[j]?.textContent
+                ?.replace(/\s{2,}/g, ' ')
+                .trim();
+              if (content.length < contentNews.length) content = contentNews;
+            }
+          }
+          pageDetails.title = title;
+          pageDetails.content = content;
+          return pageDetails;
+        }, slicedNews[i]);
+        result.push(pageData);
+        await page.close();
+      }
+      //work with result, call Damian API
+      await browser.close();
+    } catch (err) {
+      console.log(err);
+    }
+    return 'rank';
   }
 }
